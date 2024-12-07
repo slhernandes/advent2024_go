731a02e96bd9c6a50e39cc9dd1a2ad82fcb162f0
Samuel L. Hernandes <slhernandes01@gmail.com>
2024-12-07 17:33:39 +0100

    Fixed day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -10,7 +10,7 @@ import (
 type Direction int
 
 const (
-	Up Direction = iota
+	Up Direction = 1<<iota
 	Right
 	Down
 	Left
@@ -37,7 +37,16 @@ func ValidCoordinate(coord Coordinate, max_row int, max_col int) bool {
 	return coord.x >= 0 && coord.x < max_row && coord.y >= 0 && coord.y < max_col
 }
 
-func Simulate(grid []string, coord Coordinate, dir Direction, vis *map[Coordinate]Direction) (bool, error) {
+func DirToIdx(dir Direction) int {
+	ret := 0
+	cur := 1
+	for ; cur & int(dir) == 0; cur = cur<<1 {
+		ret++
+	}
+	return ret
+}
+
+func Simulate(grid []string, coord Coordinate, dir Direction, vis *map[Coordinate]Direction, uturn int) (bool, error) {
 	if len(grid) == 0 {
 		return false, errors.New("0 row received")
 	}
@@ -45,27 +54,36 @@ func Simulate(grid []string, coord Coordinate, dir Direction, vis *map[Coordinat
 		return false, errors.New("0 column received")
 	}
 
+	if uturn > 1 {
+		return true, nil
+	}
+
 	dir_cand, ok := (*vis)[coord]
 	if ok {
-		if dir_cand == dir {
+		if dir_cand & dir != 0 {
 			return true, nil
 		}
 	}
 
 	offset := []int{-1, 0, 1, 0, -1}
-	next_candidate := Coordinate{coord.x + offset[dir], coord.y + offset[dir+1]}
+	dir_idx := DirToIdx(dir)
+	next_candidate := Coordinate{coord.x + offset[dir_idx], coord.y + offset[dir_idx+1]}
 	if ValidCoordinate(next_candidate, len(grid), len(grid[0])) {
 		var res bool
 		if grid[next_candidate.x][next_candidate.y] == '#' {
-			next_dir := (dir+1)%4
-			tmp, err := Simulate(grid, coord, next_dir, vis)
+			next_dir := (dir_idx+1)%4
+			tmp, err := Simulate(grid, coord, Direction(1<<next_dir), vis, uturn)
 			if err != nil {
 				return false, err
 			}
 			res = tmp
 		} else {
-			(*vis)[coord] = dir
-			tmp, err := Simulate(grid, next_candidate, dir, vis)
+			(*vis)[coord] |= dir
+			uturn_now := uturn
+			if UTurned((*vis)[coord]) {
+				uturn_now++
+			}
+			tmp, err := Simulate(grid, next_candidate, dir, vis, uturn_now)
 			if err != nil {
 				return false, err
 			}
@@ -73,7 +91,7 @@ func Simulate(grid []string, coord Coordinate, dir Direction, vis *map[Coordinat
 		}
 		return res, nil
 	}
-	(*vis)[coord] = dir
+	(*vis)[coord] |= dir
 	return false, nil
 }
 
@@ -82,11 +100,20 @@ func FindStart(grid []string) (Coordinate, Direction, error) {
 	for x, v := range grid {
 		for y, c := range v {
 			if slices.Contains(dir, c) {
-				return Coordinate{x, y}, Direction(slices.Index(dir, c)), nil
+				return Coordinate{x, y}, Direction(1<<slices.Index(dir, c)), nil
 			}
 		}
 	}
 	return Coordinate{}, Up, errors.New("Grid does not have start")
+}
+
+func UTurned(dir Direction) bool {
+	for i := 0; i < 4; i++ {
+		if int(dir) & (1<<i) == 15 {
+			return true
+		}
+	}
+	return false
 }
 
 func PartOne(s string) (int,error) {
@@ -97,7 +124,7 @@ func PartOne(s string) (int,error) {
 		return 0, err
 	}
 
-	_, err = Simulate(grid, start, dir, &vis)
+	_, err = Simulate(grid, start, dir, &vis, 0)
 	if err != nil {
 		return 0, err
 	}
@@ -112,7 +139,7 @@ func PartOne(s string) (int,error) {
 func AlterString(s string, i int) (bool, string) {
 	r := []rune(s)
 	dir := []rune{'^', '>', 'v', '<'}
-	if r[i] != '\n' && !slices.Contains(dir, r[i]){
+	if r[i] != '\n' && !slices.Contains(dir, r[i]) && r[i] != '#' {
 		r[i] = '#'
 	} else {
 		return true, ""
@@ -121,10 +148,23 @@ func AlterString(s string, i int) (bool, string) {
 }
 
 func PartTwo(s string) (int,error) {
+
+	grid := lib.SplitFilterEmpty(s, "\n")
+	width := len(grid[0])
+	vis := make(map[Coordinate]Direction)
+	start, dir, err := FindStart(grid)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = Simulate(grid, start, dir, &vis, 0)
+	if err != nil {
+		return 0, err
+	}
+
 	ret := 0
-	fmt.Println("!!!WARNING!!!\nThis solution is O(n^4) and possibly has uncovered (yet maybe rare) edge cases.")
-	for i := range s {
-		cont, new_s := AlterString(s, i)
+	for i := range vis {
+		cont, new_s := AlterString(s, i.y + i.x * (width+1))
 		if cont {
 			continue
 		}
@@ -135,7 +175,7 @@ func PartTwo(s string) (int,error) {
 			return 0, err
 		}
 
-		res, err := Simulate(grid, start, dir, &vis)
+		res, err := Simulate(grid, start, dir, &vis, 0)
 		if err != nil {
 			return 0, err
 		}
